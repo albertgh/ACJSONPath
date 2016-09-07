9bc6bf24496dcf422512d20dd04a72f7e72c0fd2
Albert Chu <albertzyk@gmail.com>
2016-09-08 03:33:03 +0800

    update sample api

--- ACJSONPath/RootViewController.m
+++ ACJSONPath/RootViewController.m
@@ -24,7 +24,7 @@
 
 - (void)testButtonClicked:(UIButton *)sender
 {
-    NSURL *theURL = [NSURL URLWithString:@"http://www.battlenet.com.cn/api/wow/battlePet/species/258"];
+    NSURL *theURL = [NSURL URLWithString:@"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"];
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:theURL
                                                                 cachePolicy:NSURLRequestUseProtocolCachePolicy
                                                             timeoutInterval:10];
@@ -44,7 +44,7 @@
              
              NSString *testStr = [[ACJSONPath sharedInstance]
                                   getDataFromJSONObject:json
-                                  byPath:@"abilities[0].name"];
+                                  byPath:@"query.created"];
              
              self.testLabel.text = testStr;
              
